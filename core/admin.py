459bbbe52c9f1c66c45d604f59db97ffc5d0e4e8
0x03b4 <216177528+0x03B4@users.noreply.github.com>
2025-10-15 02:06:18 +0200

    Add the functionality to export and import Qualification data in the admin portal

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+
 from .models import (
     Student,
     Qualification,
@@ -18,8 +20,11 @@ class ModuleAdmin(admin.ModelAdmin):
     list_display = ('code', 'name', 'year', 'semester', 'credits')
     search_fields = ('name', 'code')
 
+class QualificationAdmin(ImportExportModelAdmin):
+    list_display = ('name', 'duration_years', 'total_credits', 'format') 
+
 admin.site.register(Student)
-admin.site.register(Qualification)
+admin.site.register(Qualification, QualificationAdmin)
 admin.site.register(Module, ModuleAdmin)
 admin.site.register(Lecturer)
 admin.site.register(QualificationModule)
